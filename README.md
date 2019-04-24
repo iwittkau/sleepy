79f7a308d3c051ede1787bb5270abd8a31b60950
Ingmar Wittkau <iwittkau@users.noreply.github.com>
2019-04-24 13:27:56 +0200

    add optional message

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ go get -u github.com/iwittkau/sleepy
 # Usage
 
 ```
-sleepy duration
+sleepy duration [message]
 ```
 
 Sleep for 10 minutes:
@@ -20,6 +20,14 @@ Sleep for 10 minutes:
 ```
 $ sleepy 10m
 [=>----------------------------------------------------------]   3.00% 09m11s
+``` 
+
+Sleep for 10 seconds with message:
+
+```
+$ sleepy 10s "ten seconds"
+ ten seconds [============================================================] 100.00%
+ 2019/04/23 19:55:10 finished "ten seconds"
 ``` 
 
 Valid time units are `ns`, `us` (or `µs`), `ms`, `s`, `m`, `h` as in Go's `time` [package](https://godoc.org/time#ParseDuration).
--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"time"
 
 	"gopkg.in/cheggaaa/pb.v1"
 )
 
 var (
-	usage = `  usage: sleepy duration
+	usage = `  usage: sleepy duration [message]
+
 	 %s
-	 example: sleepy 10s (sleeps for 10 seconds)
+	
+	 examples: 
+	 	  sleepy 10s 
+	          	sleeps for 10 seconds
+		   
+		  sleepy 10s "sleeping 10 seconds" 
+		  	sleeps for 10 seconds and displays 
+			"sleeping 10 seconds" as the progress bar prefix
+			and a message when sleeping has finished
 `
 	validUnits = "valid time units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'"
 )
@@ -22,6 +33,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	var prefix string
+	if len(os.Args) > 2 {
+		prefix = fmt.Sprintf(" %s ", os.Args[2])
+		log.SetFlags(log.Ltime | log.Ldate)
+	}
+
+	defer printJobName(prefix)
+
 	d, err := time.ParseDuration(os.Args[1])
 	if err != nil {
 		fmt.Println(err.Error())
@@ -31,11 +50,12 @@ func main() {
 
 	if d.Nanoseconds() < time.Second.Nanoseconds() {
 		time.Sleep(d)
-		os.Exit(0)
+		return
 	}
 
 	count := d.Nanoseconds() / time.Second.Nanoseconds()
 	bar := pb.StartNew(int(count))
+	bar.Prefix(prefix)
 	bar.ShowCounters = false
 	bar.ShowTimeLeft = true
 	bar.SetUnits(pb.U_DURATION)
@@ -43,5 +63,11 @@ func main() {
 		bar.Increment()
 		time.Sleep(time.Second)
 	}
+}
 
+func printJobName(s string) {
+	if s != "" {
+		s = strings.TrimSpace(s)
+		log.Printf("finished %q\n", s)
+	}
 }
